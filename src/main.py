06e634d0379b3321e2d5eff7eb3210f921c068a9
Kristian Hannula <kristian.hannula@proton.me>
2024-10-15 14:33:20 +0300

    better reponame parsing

--- src/main.py
+++ src/main.py
@@ -6,7 +6,9 @@ df = pd.read_csv("sonar_measures.csv", low_memory=False, dtype={7: str, 8: str})
 project_names = df["project"]
 
 project_names = project_names.apply(
-    lambda project: project.removeprefix("apache").removeprefix("_")
+    lambda project: project.removeprefix("apache_")
+    .replace("-master", "")
+    .removeprefix("apache-")
 )
 
 project_names = project_names.drop_duplicates()
