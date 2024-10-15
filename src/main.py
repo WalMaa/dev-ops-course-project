4201c1cdf51976238c48df27797fc04ac8f89f9a
Kristian Hannula <kristian.hannula@proton.me>
2024-10-15 13:01:11 +0300

    simple github url snatcher

---
+++ src/main.py
@@ -0,0 +1,27 @@
+import pandas as pd
+
+
+df = pd.read_csv("sonar_measures.csv", low_memory=False, dtype={7: str, 8: str})
+
+project_names = df["project"]
+
+project_names = project_names.apply(
+    lambda project: project.removeprefix("apache").removeprefix("_")
+)
+
+project_names = project_names.drop_duplicates()
+
+github_urls = []
+
+for project in project_names:
+    url = f"https://github.com/apache/{project}"
+    github_urls.append(url)
+
+
+def main():
+    for url in github_urls:
+        print(url)
+
+
+if __name__ == "__main__":
+    main()
