55e9fe3c0d4fcf1468c20e4eee1333cb3bc3f40f
WalMaa <98175031+WalMaa@users.noreply.github.com>
2024-11-03 19:14:52 +0200

    initialized file

--- src/main.py
+++ src/main.py
@@ -9,6 +9,7 @@ import refactoring_activity_analyzer
 import refactoring_miner
 import repository_cloner
 import repository_fetcher
+import refactoring_developers_effort
 from util import LogLevel, log_and_print
 
 
@@ -52,14 +53,17 @@ async def main():
     )
 
     # Comment / uncomment the rows below based on what you want to do
-    await repository_fetcher.get_repositories(csv_file, semaphore)
-    await repository_cloner.clone(
-        cloned_repositories_dir,
-    )
-    await refactoring_miner.run_miner(
-        cloned_repositories_dir, refactoring_miner_exec, semaphore
-    )
-    await refactoring_activity_analyzer.analyze(cloned_repositories_dir, semaphore)
+    # await repository_fetcher.get_repositories(csv_file, semaphore)
+    # await repository_cloner.clone(
+    #     cloned_repositories_dir,
+    # )
+    # await refactoring_miner.run_miner(
+    #     cloned_repositories_dir, refactoring_miner_exec, semaphore
+    # )
+    # await refactoring_activity_analyzer.analyze(cloned_repositories_dir, semaphore)
+    refactoring_developers_effort.calculate()
+    
+    
 
 
 if __name__ == "__main__":
---
+++ src/refactoring_developers_effort.py
@@ -0,0 +1,9 @@
+"""
+This module is responsible for refactoring the developers effort.
+
+
+
+"""
+
+def calculate():
+    print("refactoring_developers_effort.py")
